33fada4cc0f5b3e3d0e7e9e9640801509b7a1606
Pradeep Kancharla <q4pradeep@gmail.com>
2025-12-28 03:37:04 +0100

    Bump version to 1.0.1

--- build_app.sh
+++ build_app.sh
@@ -77,7 +77,7 @@ cat > "$APP_BUNDLE/Contents/Info.plist" <<EOF
     <key>CFBundlePackageType</key>
     <string>APPL</string>
     <key>CFBundleShortVersionString</key>
-    <string>1.0.0</string>
+    <string>1.0.1</string>
     <key>CFBundleVersion</key>
     <string>1</string>
     <key>LSMinimumSystemVersion</key>
